5d270411557b25abf17be6823bed756942c7daed
Steven Chen <qwe16165850@gmail.com>
2018-09-08 14:17:35 +1000

    add babel-plugin-import

--- blockchain-demo/src/index.js
+++ blockchain-demo/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { DatePicker } from 'antd';
 import registerServiceWorker from './registerServiceWorker';
 
+ReactDOM.render(<DatePicker />, mountedNode);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
